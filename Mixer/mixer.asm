8ad7e53b56fa745838a6942a049afa987003968b
Jeroen Knoester <powerprograms@powerprograms.nl>
2025-06-19 12:02:02 +0200

    Corrected channel return value for MixerPlay<all>

--- Mixer/mixer.asm
+++ Mixer/mixer.asm
@@ -2793,7 +2793,7 @@ MixerChannelWrite\1
 			IF MIXER_C_DEFS=1
 				movem.l	d0/d1/a0/a1/a2,-(sp)
 			ELSE
-				move.l	a1,-(sp)
+				movem.l	d0/a1,-(sp)
 			ENDIF
 			
 			lea.l	mixer_irqdma_vectors\1(pc),a1
@@ -2826,7 +2826,7 @@ MixerChannelWrite\1
 			IF MIXER_C_DEFS=1
 				movem.l	(sp)+,d0/d1/a0/a1/a2
 			ELSE
-				move.l	(sp)+,a1
+				movem.l	(sp)+,d0/a1
 			ENDIF
 		ENDIF
 .irq_disabled
