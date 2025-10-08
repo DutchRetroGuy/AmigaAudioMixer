aa07f1778d2e269f834955be47c0a7d3ef2dde8c
DutchRetroGuy <powerprograms@powerprograms.nl>
2025-10-08 17:50:35 +0200

    Fixed A2 being trashed in some circumstances

--- Mixer/mixer.asm
+++ Mixer/mixer.asm
@@ -2850,7 +2850,7 @@ MixerChannelWrite\1
 			IF MIXER_C_DEFS=1
 				movem.l	d0/d1/a0/a1/a2,-(sp)
 			ELSE
-				movem.l	d0/a1,-(sp)
+				movem.l	d0/a1/a2,-(sp)
 			ENDIF
 			
 			lea.l	mixer_irqdma_vectors\1(pc),a1
@@ -2883,7 +2883,7 @@ MixerChannelWrite\1
 			IF MIXER_C_DEFS=1
 				movem.l	(sp)+,d0/d1/a0/a1/a2
 			ELSE
-				movem.l	(sp)+,d0/a1
+				movem.l	(sp)+,d0/a1/a2
 			ENDIF
 		ENDIF
 .irq_disabled
